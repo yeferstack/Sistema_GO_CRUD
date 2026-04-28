61c9891c6901e36b3eeb691bee1a3ff20179b76d
Yefer <yefersonnjar02192510@gmail.com>
2026-04-28 16:08:03 -0500

    modelos de notificación y historial de administrador

---
+++ models/historial_admin.go
@@ -0,0 +1,16 @@
+package models
+
+import "time"
+
+type HistorialAdmin struct {
+	ID_Historial      int       `json:"id_historial"`
+	ID_Admin          int       `json:"id_admin"`
+	Accion            string    `json:"accion"`
+	Descripcion       string    `json:"descripcion"`
+	TipoObjeto        string    `json:"tipo_objeto"`
+	ID_Objeto         *int      `json:"id_objeto"`
+	FechaAccion       time.Time `json:"fecha_accion"`
+	Activo            bool      `json:"activo"`
+	FechaCreacion     time.Time `json:"fecha_creacion"`
+	FechaModificacion time.Time `json:"fecha_modificacion"`
+}
