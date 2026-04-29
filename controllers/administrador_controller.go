26edf0f03cb6af12948dfdf25582eb855014e23f
Yefer <yefersonnjar02192510@gmail.com>
2026-04-28 23:12:42 -0500

    creación del controlador y consulta de administradores

--- controllers/administrador_controller.go
+++ controllers/administrador_controller.go
@@ -9,3 +9,23 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// GET ALL Administrador
+func GetAllAdministrador(w http.ResponseWriter, r *http.Request) {
+	rows, err := config.DB.Query(`
+		SELECT id_admin, id_usuario, id_nivelacceso, activo, fecha_asignacion, fecha_creacion, fecha_modificacion 
+		FROM "Sistema"."Administrador"`)
+	if err != nil {
+		respondJSON(w, 500, map[string]string{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	var list []models.Administrador
+	for rows.Next() {
+		var a models.Administrador
+		rows.Scan(&a.ID_Admin, &a.ID_Usuario, &a.ID_NivelAcceso, &a.Activo, &a.FechaAsignacion, &a.FechaCreacion, &a.FechaModificacion)
+		list = append(list, a)
+	}
+	respondJSON(w, 200, list)
+}
+
