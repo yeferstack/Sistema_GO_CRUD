892321d1b18e1f506cdbd2db3520a32a3bdd3bb5
Yefer <yefersonnjar02192510@gmail.com>
2026-04-28 23:18:28 -0500

    eliminación de administrador-permiso

--- controllers/administrador_permiso_controller.go
+++ controllers/administrador_permiso_controller.go
@@ -79,3 +79,14 @@ func UpdateAdminPermiso(w http.ResponseWriter, r *http.Request) {
 	}
 	respondJSON(w, 200, map[string]string{"message": "AdministradorPermiso actualizado correctamente"})
 }
+
+// DELETE AdministradorPermiso
+func DeleteAdminPermiso(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	_, err := config.DB.Exec(`DELETE FROM "Sistema"."AdministradorPermiso" WHERE id=$1`, id)
+	if err != nil {
+		respondJSON(w, 500, map[string]string{"error": err.Error()})
+		return
+	}
+	respondJSON(w, 200, map[string]string{"message": "AdministradorPermiso eliminado correctamente"})
+}
