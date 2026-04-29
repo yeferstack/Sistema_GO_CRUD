d18f206ee759c56e268d3c770e5140dccab63871
Yefer <yefersonnjar02192510@gmail.com>
2026-04-29 00:15:28 -0500

    agregar config, corregir estructura de paquetes

---
+++ models/administrador_permiso.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type AdministradorPermiso struct {
+	ID                int       `json:"id"`
+	ID_Admin          int       `json:"id_admin"`
+	ID_Permiso        int       `json:"id_permiso"`
+	Activo            bool      `json:"activo"`
+	FechaCreacion     time.Time `json:"fecha_creacion"`
+	FechaModificacion time.Time `json:"fecha_modificacion"`
+}
